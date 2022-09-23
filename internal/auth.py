a83d4d113eaf329ab689d10319bb2d484ad126e2
Niek <Frankenstein795@gmail.com>
2022-09-23 14:01:11 +0200

    Use google decoder

--- internal/auth.py
+++ internal/auth.py
@@ -1,6 +1,9 @@
 import jwt
+from dotenv import dotenv_values
 from fastapi import Depends
 from fastapi.security import HTTPBearer
+from google.auth.transport import requests
+from google.oauth2 import id_token
 
 from internal.models.betting.user import BaseUser
 
@@ -9,13 +12,17 @@ from internal.models.betting.user import BaseUser
 
 token_auth_scheme = HTTPBearer()
 
+config = dotenv_values(".env")
+
 
 def decode_token(token):
-    token = token.credentials
-    user = jwt.decode(token, options={"verify_signature": False})
+    request = requests.Request()
+
+    user = id_token.verify_oauth2_token(token.credentials, request, audience=config["GOOGLE_ID"])
+
     return BaseUser(username=user["name"].lower(), uuid=user["sub"])
 
 
-def decode_user(token: str = Depends(token_auth_scheme)):
+def decode_user(token: jwt = Depends(token_auth_scheme)):
     user = decode_token(token)
     return user
