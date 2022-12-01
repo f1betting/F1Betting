fcae84519d8cb71112f03176454c9a9f388426d1
Niek <Frankenstein795@gmail.com>
2022-12-01 15:07:41 +0100

    Add unit test, update action with secrets

---
+++ tests/mock_data/mock_users.py
@@ -0,0 +1,9 @@
+def get_all_users_data():
+    return {
+        "users": [
+            {
+                "username": "test_user",
+                "uuid": "1"
+            }
+        ]
+    }
---
+++ tests/test_users.py
@@ -0,0 +1,19 @@
+import unittest
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+from tests.mock_data.mock_users import get_all_users_data
+
+
+class TestRoundPoints(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.test_client = TestClient(app, base_url="http://127.0.0.1:8000")
+
+    def test_get_all_users(self):
+        res = self.test_client.get("/users").json()
+
+        mock_data = get_all_users_data()
+
+        self.assertEqual(res, mock_data)
